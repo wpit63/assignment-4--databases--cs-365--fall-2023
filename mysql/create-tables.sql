6d686bb6c95b14d6e299192bd3caf33d04f69fc3
wpit63 <73858845+wpit63@users.noreply.github.com>
2023-12-17 16:07:57 -0500

    Update create-tables.sql
    Modified to fit ER diagram and added relation tables

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -5,22 +5,38 @@ CREATE TABLE IF NOT EXISTS artist (
 );
 
 CREATE TABLE IF NOT EXISTS album (
-  artist_id  INT          NOT NULL,
+  artist_id  INT          REFERENCES artist(artist_id),
   album_id   INT          NOT NULL,
   album_name VARCHAR(128) NOT NULL,
-  PRIMARY KEY (artist_id, album_id)
+  PRIMARY KEY (album_id, album_name)
 );
 
 CREATE TABLE IF NOT EXISTS track (
-  artist_id  INT          NOT NULL,
-  album_id   INT          NOT NULL,
+  artist_id  INT          REFERENCES artist(artist_id),
+  album_id   INT          REFERENCES album(album_id),
   track_id   INT          NOT NULL,
   track_name VARCHAR(128) NOT NULL,
   time       DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (artist_id, album_id, track_id)
+  PRIMARY KEY (track_id, track_name)
+);
+
+CREATE TABLE IF NOT EXISTS compiles (
+  artist_name  INT          REFERENCES artist(artist_name),
+  album_name   INT          REFERENCES album(album_name)
+);
+
+CREATE TABLE IF NOT EXISTS contains (
+  album_name   INT          REFERENCES album(album_name),
+  track_name   INT          REFERENCES track(track_name)
+);
+
+CREATE TABLE IF NOT EXISTS wasPlayedAt (
+  album_name   INT           REFERENCES album(album_name),
+  playTime     INT           REFERENCES played(played)
 );
 
 CREATE TABLE IF NOT EXISTS played (
-  played     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  album_name VARCHAR(128)  NOT NULL
+  played     TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (played)
 );
