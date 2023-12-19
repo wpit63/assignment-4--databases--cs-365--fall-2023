dfbe1af1cf89054577477a47ff8a5eeba975e2e5
wpit63 <73858845+wpit63@users.noreply.github.com>
2023-12-19 14:31:45 -0500

    Update create-tables.sql
    (Hopefully) finish modifying table columns

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -5,38 +5,40 @@ CREATE TABLE IF NOT EXISTS artist (
 );
 
 CREATE TABLE IF NOT EXISTS album (
-  artist_id  INT          REFERENCES artist(artist_id),
+  artist_id  INT          NOT NULL,
   album_id   INT          NOT NULL,
   album_name VARCHAR(128) NOT NULL,
-  PRIMARY KEY (album_id, album_name)
+  PRIMARY KEY (artist_id, album_id),
+  FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS track (
-  artist_id  INT          REFERENCES artist(artist_id),
-  album_id   INT          REFERENCES album(album_id),
+  artist_id  INT          NOT NULL,
+  album_id   INT          NOT NULL,
   track_id   INT          NOT NULL,
   track_name VARCHAR(128) NOT NULL,
   time       DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (track_id, track_name)
+  PRIMARY KEY (artist_id, album_id, track_id),
+  FOREIGN KEY (artist_id, album_id) REFERENCES album(artist_id, album_id)
 );
 
 CREATE TABLE IF NOT EXISTS compiles (
-  artist_name  INT          REFERENCES artist(artist_name),
-  album_name   INT          REFERENCES album(album_name)
+  artist_name  VARCHAR(128) REFERENCES artist(artist_name),
+  album_name   VARCHAR(128) REFERENCES album(album_name),
+  artist_id    INT          REFERENCES artist(artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS contains (
-  album_name   INT          REFERENCES album(album_name),
-  track_name   INT          REFERENCES track(track_name)
-);
-
-CREATE TABLE IF NOT EXISTS wasPlayedAt (
-  album_name   INT           REFERENCES album(album_name),
-  playTime     INT           REFERENCES played(played)
+  album_name   VARCHAR(128) REFERENCES album(album_name),
+  track_name   VARCHAR(128) REFERENCES track(track_name)
 );
 
 CREATE TABLE IF NOT EXISTS played (
-  album_name VARCHAR(128)  NOT NULL
+  track_name VARCHAR(128)  REFERENCES contains(track_name),
   played     TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (played)
 );
+
+CREATE UNIQUE INDEX idx_artist ON artist(artist_id);
+CREATE UNIQUE INDEX idx_album ON album(artist_id, album_id);
+CREATE UNIQUE INDEX idx_track ON track(artist_id, album_id, track_id);
