e43a84c7687a4112537c83c2cb36213378170325
wpit63 <73858845+wpit63@users.noreply.github.com>
2023-12-19 15:41:43 -0500

    Update commands.sql
    added query for second command

--- commands.sql
+++ commands.sql
@@ -1,9 +1,10 @@
-
 -- shows all entries in the compiles relation with concatenation
 SELECT CONCAT(artist_name, " recorded ", album_name) AS Recording FROM compiles;
 
 -- shows the expected output using the subquery
-SELECT artist_name FROM artist WHERE artist_id = ();
+-- result equivalent to running:
+-- SELECT artist_name, album_name FROM artist, album WHERE artist.artist_id = album.artist_id;
+SELECT artist_name, album_name FROM artist JOIN album ON artist.artist_id = album.artist_id WHERE artist.artist_id IN (SELECT album.artist_id FROM album);
 
 -- shows the expected output user the compiles relation
 SELECT artist_name, album_name FROM compiles ORDER BY artist_id ASC;
